03755233a922d64b8a306866cdf011dfcea920bb
Jasterix C <loune.calixte@gmail.com>
2019-04-13 03:03:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     lang= programmer_hash[:dennis_ritchie][:languages][0]
+     programmer_hash[:dennis_ritchie][:languages][0]
      
 end
 
